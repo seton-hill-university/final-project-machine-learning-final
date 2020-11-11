dc8e13719922e361ab8115a4cb30b81a0f8a1c24
cvoorhees15 <54859729+cvoorhees15@users.noreply.github.com>
2020-11-11 11:21:20 -0500

    entropy calculator and R^2 calculator

--- main.py
+++ main.py
@@ -1,67 +1,31 @@
-# Ashly DeFalco
-# final project
-# binning
-
-import csv
 import pandas as pd
-file = 'fileData.csv'
+import math
+import numpy as np
 
-frame = pd.DataFrame()
+data = pd.read_excel('/Users/c.voorhees/Desktop/finalData2.xlsx', 'Sheet 1')
+print(data)
 
-def raw_data():
-    raw_data = []
-    with open("finalData.csv", "rb") as rawfile:
-        reader = csv.reader(rawfile, delimiter=",")
-        next(reader)
-        for column in reader:
-            raw_data.append(column)
 
-    raw_data = pd.DataFrame(raw_data)
-    return raw_data.T
+# Entropy of server data
+totalItems = 786384
+uniqueItems = 12
+divNumber = 65532
 
-#bin 1: allow
-bin_allow_index=[]
-for i in range (raw_data):
-    if raw_data['Action'].iloc[i] == 'allow':
-        bin_allow_index.append(i)
-bin_allow=[]
-for i in bin_allow_index:
-    bin_allow.append(raw_data.iloc[i])
-bin_allow = pd.DataFreame(bin_allow)
-bin_allow
+print("Entropy")
+print((-((divNumber / totalItems) * math.log((divNumber / totalItems), 2))) * uniqueItems)
 
-#bin 2: dany
-bin_deny_index=[]
-for i in range (raw_data):
-    if raw_data['Action'].iloc[i] == 'deny':
-        bin_deny_index.append(i)
-bin_deny=[]
-for i in bin_deny_index:
-    bin_deny.append(raw_data.iloc[i])
-bin_deny = pd.DataFreame(bin_deny)
-bin_deny
 
-#bin 3: drop
-bin_drop_index=[]
-for i in range (raw_data):
-    if raw_data['Action'].iloc[i] == 'drop':
-        bin_drop_index.append(i)
-bin_drop=[]
-for i in bin_drop_index:
-    bin_drop.append(raw_data.iloc[i])
-bin_drop = pd.DataFreame(bin_drop)
-bin_drop
+# Set x and y values
+xValues = (data['Bytes Sent'])
+yValues = (data['Bytes Received'])
 
-#bin 4: reset-both
-bin_reset_index=[]
-for i in range (raw_data):
-    if raw_data['Action'].iloc[i] == 'reset':
-        bin_reset_index.append(i)
-bin_reset=[]
-for i in bin_reset_index:
-    bin_reset.append(raw_data.iloc[i])
-bin_reset = pd.DataFreame(bin_reset)
-bin_reset
+# Calculate R and R squared of bytes sent and bytes received
+correlation_matrix = np.corrcoef(xValues, yValues)
+correlation_xy = correlation_matrix[0,1]
+r = correlation_xy
+r_squared = correlation_xy**2
+print("\nR:")
+print(r)
+print("\nR squared:")
+print(r_squared)
 
-raw_data=pd.read_csv(file, 'Action')
-print(raw_data)
