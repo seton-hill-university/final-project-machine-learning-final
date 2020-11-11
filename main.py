790d59f9d19c0d637d35f9c3ba5976f9cef87fb0
Germaine Uwimpuhwe <49528936+germaine12@users.noreply.github.com>
2020-11-11 11:07:01 -0500

    Add files via upload
    Dimensionality Reduction.

--- main.py
+++ main.py
@@ -1,17 +1,65 @@
+# importing libraries
+
 import pandas as pd
-import random
-file = 'finalData.csv'
+import numpy as np
+import matplotlib.pyplot as plt
 
-def sample():
-    #counting lines
-    lines = sum(1 for i in open(file))
-    #samplesize 10 x
-    size = int(lines * 0.1)
-    #Randomly skips rows. Only keep size rows. Keeps the header
-    skip = random.sample(range(1,lines), lines - size)
 
-    #removes every row in skip. faster loadtime
-    data = pd.read_csv(file, skiprows=skip)
-    print(data)
+df= pd.read_csv ('finalData.csv')
 
-sample()
+
+# I used two different dimentionality reduction.
+# The below one is Random Forest
+# It works with only numeric values
+# This method shows you what feature are more important than the other
+# And you select features based on their weight
+from sklearn.ensemble import RandomForestRegressor
+
+# Dropping action column because it's not numeric variable
+df=df.drop(['Action'], axis=1)
+model = RandomForestRegressor(random_state=1, max_depth=10)
+df=pd.get_dummies(df)
+model.fit(df,df.Bytes)
+
+features = df.columns
+importances = model.feature_importances_
+indices = np.argsort(importances)[-9:]  # top 10 features
+plt.title('Feature Importances')
+plt.barh(range(len(indices)), importances[indices], color='b', align='center')
+plt.yticks(range(len(indices)), [features[i] for i in indices])
+plt.xlabel('Relative Importance')
+plt.show()
+
+from sklearn.feature_selection import SelectFromModel
+feature = SelectFromModel(model)
+Fit = feature.fit_transform(df, df.Bytes)
+print(Fit)
+
+#After using this graph shows that NAT source port column is the least important.
+
+
+##########################
+#This is Low Variance Filter method that shows you what features that can be removed and don't impact our dataset a lot.
+
+n = df.isnull().sum()/len(df)*100
+print(n)
+
+#calculating variance of all numeric variables.
+m = df.var()
+
+num = df.var()
+print(num)
+
+# Due to Variance values of sttributes, I will drop all atributes less than 2 because they have low variance to my dataset.
+#looking on how many columns I have before dropping some of them.
+print(len(df.columns))
+df= df.drop(['Bytes Sent','pkts_sent'], axis=1)
+#Counting columns and see if I was able to drop targeted columns
+print(len(df.columns))
+
+print(len(df))
+
+
+
+
+
