215d72d8be1a622037ce98fca83d444ee9483426
ashdef <69993346+ashdef@users.noreply.github.com>
2020-11-23 10:23:19 -0500

    Add files via upload
    binning

--- main.py
+++ main.py
@@ -1,31 +1,67 @@
+# Ashly DeFalco
+# final project
+# binning
+
+import csv
 import pandas as pd
-import math
-import numpy as np
+file = 'fileData.csv'
 
-data = pd.read_excel('/Users/c.voorhees/Desktop/finalData2.xlsx', 'Sheet 1')
-print(data)
+frame = pd.DataFrame()
 
+def raw_data():
+    raw_data = []
+    with open("finalData.csv", "rb") as rawfile:
+        reader = csv.reader(rawfile, delimiter=",")
+        next(reader)
+        for column in reader:
+            raw_data.append(column)
 
-# Entropy of server data
-totalItems = 786384
-uniqueItems = 12
-divNumber = 65532
+    raw_data = pd.DataFrame(raw_data)
+    return raw_data.T
 
-print("Entropy")
-print((-((divNumber / totalItems) * math.log((divNumber / totalItems), 2))) * uniqueItems)
+#bin 1: allow
+bin_allow_index=[]
+for i in range (len(raw_data)):
+    if raw_data['Action'].iloc[i] == 'allow':
+        bin_allow_index.append(i)
+bin_allow=[]
+for i in bin_allow_index:
+    bin_allow.append(raw_data.iloc[i])
+bin_allow = pd.DataFreame(bin_allow)
+bin_allow
 
+#bin 2: dany
+bin_deny_index=[]
+for i in range (len(raw_data)):
+    if raw_data['Action'].iloc[i] == 'deny':
+        bin_deny_index.append(i)
+bin_deny=[]
+for i in bin_deny_index:
+    bin_deny.append(raw_data.iloc[i])
+bin_deny = pd.DataFreame(bin_deny)
+bin_deny
 
-# Set x and y values
-xValues = (data['Bytes Sent'])
-yValues = (data['Bytes Received'])
+#bin 3: drop
+bin_drop_index=[]
+for i in range (len(raw_data)):
+    if raw_data['Action'].iloc[i] == 'drop':
+        bin_drop_index.append(i)
+bin_drop=[]
+for i in bin_drop_index:
+    bin_drop.append(raw_data.iloc[i])
+bin_drop = pd.DataFreame(bin_drop)
+bin_drop
 
-# Calculate R and R squared of bytes sent and bytes received
-correlation_matrix = np.corrcoef(xValues, yValues)
-correlation_xy = correlation_matrix[0,1]
-r = correlation_xy
-r_squared = correlation_xy**2
-print("\nR:")
-print(r)
-print("\nR squared:")
-print(r_squared)
+#bin 4: reset-both
+bin_reset_index=[]
+for i in range (len(raw_data)):
+    if raw_data['Action'].iloc[i] == 'reset':
+        bin_reset_index.append(i)
+bin_reset=[]
+for i in bin_reset_index:
+    bin_reset.append(raw_data.iloc[i])
+bin_reset = pd.DataFreame(bin_reset)
+bin_reset
 
+raw_data=pd.read_csv(file, 'Action')
+print(len(raw_data))
